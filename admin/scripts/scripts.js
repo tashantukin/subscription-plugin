8c09bbadd9d892f93ef82d3663fb7b30cf7b3400
Natasha Maruska Navarro <nmfnavarro@gmail.com>
2021-05-10 09:28:14 +0800

    fixed for Date format settings

--- admin/scripts/scripts.js
+++ admin/scripts/scripts.js
@@ -9,9 +9,21 @@
   var re = /([a-f0-9]{8}(?:-[a-f0-9]{4}){3}-[a-f0-9]{12})/i;
   var packageId = re.exec(scriptSrc.toLowerCase())[1];
   const HOST = window.location.host;
-  
+  const protocol = window.location.protocol;
+  const token = getCookie('webapitoken');
+  const baseURL = window.location.hostname;
+  var day, month, year;
+
   const url = window.location.href.toLowerCase();
   //get coupon value to display in Admin transaction details page
+
+  function getCookie(name){
+  var value = '; ' + document.cookie;
+  var parts = value.split('; ' + name + '=');
+  if (parts.length === 2) {
+      return parts.pop().split(';').shift();
+  }
+}
 
   function waitForElement(elementPath, callBack)
   {
@@ -43,8 +55,8 @@
 
                 var dateDetails = result != null || result != "" ? JSON.parse(result) : null;
                 
-                var startdate = dateDetails.start == null ? "" : new Date(dateDetails.start * 1000).format("dd/mm/yyyy");
-                var enddate = dateDetails.end == null ? "" : new Date(dateDetails.end * 1000).format("dd/mm/yyyy");
+                var startdate = dateDetails.start == null ? "" : new Date(dateDetails.start * 1000).format(`${day}/${month}/${year}`);
+                var enddate = dateDetails.end == null ? "" : new Date(dateDetails.end * 1000).format(`${day}/${month}/${year}`);
                 var separator = dateDetails.start == null ? "" : '-';
                 el.text(`${startdate} ${separator} ${enddate}`);
               
@@ -54,17 +66,46 @@
 				// toastr.error('Error!');
 			}
 		});
-    }
+  }
+  
+
+  function getDateFormat()
+  {
+    axios({
+      method: "GET",
+      url: `${protocol}//${baseURL}/api/v2/marketplaces/`,
+      headers: {
+        'Authorization': `Bearer ${token}`
+      }
+    }).then((response) =>
+    {
+      var dateFormat = response.data.Settings['features-control']['date-format-settings'];
+
+     day = dateFormat['day'].toLowerCase();
+     month = dateFormat['month'].toLowerCase();
+     year = dateFormat['year'].toLowerCase();
+
+      console.log(dateFormat);
+
+      
+    })
+  }
   $(document).ready(function ()
   {
     //   if (url.indexOf("/admin/usermanager/") >= 0) {
 
-      if ($('body').hasClass('user-page')) {
+    if ($('body').hasClass('user-page')) {
+        
+      var axiosCDN = `<script src="https://cdnjs.cloudflare.com/ajax/libs/axios/0.19.2/axios.js"></script>`;
+      $('body').append(axiosCDN);
+      // document.head.appendChild(axiosCDN); 
+ 
+
           console.log('userpage')
 
             waitForElement("#no-more-tables", function ()
             {
-
+              getDateFormat();
               //append the header
               
               var billing = `<th data-column="6" tabindex="0" scope="col" role="columnheader" aria-disabled="false" aria-controls="no-more-tables" unselectable="on" aria-sort="none" aria-label="User Type: No sort applied, activate to apply an ascending sort" style="user-select: none;"><div class="tablesorter-header-inner">Billing Cycle</div></th>`;
